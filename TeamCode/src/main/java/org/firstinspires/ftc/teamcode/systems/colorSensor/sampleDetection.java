7392a54acdc70dc4e9e2253b3047ea04f1fafd49
davisod <david.andreescu.sound@gmail.com>
2025-07-22 23:11:57 +0300

    add color detection for sample (not tested on robot, also, robot seems to not recognize the CSS sensor)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/colorSensor/sampleDetection.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.systems.colorSensor;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+import android.graphics.Color;
+
+import static org.firstinspires.ftc.teamcode.systems.colorSensor.colorConfig.AllianceColor.*;
+
+public class sampleDetection {
+
+    private final NormalizedColorSensor colorSensor;
+
+    public sampleDetection(HardwareMap hardwareMap) {
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "CSS");
+
+        if (colorSensor instanceof SwitchableLight) {
+            ((SwitchableLight) colorSensor).enableLight(true);
+        }
+    }
+
+    public boolean checkColor() {
+        float[] hsv = new float[3];
+        Color.colorToHSV(colorSensor.getNormalizedColors().toColor(), hsv);
+
+        colorConfig.AllianceColor alliance = colorConfig.alliance;
+
+        // RED alliance: detect RED or YELLOW samples
+        if (alliance == RED) {
+            return isRed(hsv) || isYellow(hsv);
+        }
+
+        // BLUE alliance: detect BLUE or YELLOW samples
+        return isBlue(hsv) || isYellow(hsv);
+    }
+
+    private boolean isRed(float[] hsv) {
+        return hsv[0] >= 0 && hsv[0] <= 30;
+    }
+
+    private boolean isBlue(float[] hsv) {
+        return hsv[0] >= 210 && hsv[0] <= 270;
+    }
+
+    private boolean isYellow(float[] hsv) {
+        return hsv[0] >= 30 && hsv[0] <= 70;
+    }
+}
